49429f303198a69783fd39339367e3a88e636c0d
Hannah Haken <hannahhaken@gmail.com>
2017-04-24 20:53:05 +0100

    Changed board test to get google homepage

--- tests/boards/board_resource.py
+++ tests/boards/board_resource.py
@@ -1,6 +1,8 @@
 import unittest
+import requests
 
 
 class BoardResource(unittest.TestCase):
     def test_can_create_new_board(self):
-        self.assertEqual(1, 1)
+        response = requests.get('https://www.google.com')
+        self.assertEqual(200, response.status_code)
