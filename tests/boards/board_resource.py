387d97825c10d5baf40dbf329cb94ccbcc4c4eba
Hannah Haken <hannahhaken@gmail.com>
2017-05-29 15:29:51 +0100

    Add a test to update board

--- tests/boards/board_resource.py
+++ tests/boards/board_resource.py
@@ -6,18 +6,17 @@ from tests.configuration.trello_token import TOKEN
 class BoardResource(unittest.TestCase):
     TRELLO_BASE_URL = 'https://api.trello.com/1'
 
-    def test_can_view_the_test_board(self):
+    def test_can_view_an_existing_board(self):
         # GIVEN
         board_id = 'PJ0OHfNl'
 
         # WHEN
-        response = requests.get(
-            f'{self.TRELLO_BASE_URL}/boards/{board_id}/lists?token={TOKEN}')
+        response = self.get_board(board_id)
 
         # THEN
-        trello_columns = response.json()
+        board = response.json()
 
-        self.assertEqual(trello_columns[0]['id'], '5772e42314ea86e7d305bade')
+        self.assertEqual(board['name'], 'Welcome Board')
 
     def test_can_create_new_board(self):
         # GIVEN
@@ -28,22 +27,43 @@ class BoardResource(unittest.TestCase):
 
         self.delete_board(create_board_result['board_id'])
 
-    # def test_can_update_board_name(self):
+    def test_can_update_board_name(self):
+        # GIVEN
+        new_board = self.create_new_board('Test can update board name', 'description')
+        board_id = new_board['board_id']
 
-    def create_new_board(self, name, description):
+        response = requests.put(f'{self.TRELLO_BASE_URL}/boards/{board_id}/name?token={TOKEN}', json={
+            'value': 'new board name'
+        })
+
+        self.assertEqual(response.status_code, 200)
+
+        board = self.get_board(board_id).json()
+
+        self.assertEqual(board['name'], 'new board name')
+
+        self.delete_board(board_id)
+
+    @staticmethod
+    def create_new_board(name, description):
         new_trello_board = {
             'name': name,
             'desc': description
         }
-        response = requests.post(f'{self.TRELLO_BASE_URL}/boards?token={TOKEN}', json=new_trello_board)
+        response = requests.post(f'{BoardResource.TRELLO_BASE_URL}/boards?token={TOKEN}', json=new_trello_board)
 
         return {
             'board_id': response.json()['id'],
             'status_code': response.status_code
         }
 
+    @staticmethod
+    def get_board(board_id):
+        response = requests.get(f'{BoardResource.TRELLO_BASE_URL}/boards/{board_id}?token={TOKEN}')
+        return response
+
     def delete_board(self, board_id):
         response = requests.delete(
-            f'{self.TRELLO_BASE_URL}/boards/{board_id}?token={TOKEN}')
+            f'{BoardResource.TRELLO_BASE_URL}/boards/{board_id}?token={TOKEN}')
 
         self.assertEqual(response.status_code, 200)
