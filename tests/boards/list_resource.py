17e9372b0d84e9646f175fc22791f0a3fd803345
Hannah Haken <hannahhaken@gmail.com>
2017-06-04 16:59:54 +0100

    Add update list name test

--- tests/boards/list_resource.py
+++ tests/boards/list_resource.py
@@ -19,6 +19,26 @@ class ListResource(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.delete_board(board_id)
 
+    def test_can_update_list_name(self):
+        created_board_result = self.create_new_board('can create list', 'description')
+        board_id = created_board_result['board_id']
+
+        create_list_response = requests.post(f'{ListResource.TRELLO_BASE_URL}/lists?token={TOKEN}', json={
+            'name': 'new list',
+            'idBoard': board_id
+        })
+
+        list_id = create_list_response.json()['id']
+
+        update_list_response = requests.put(f'{ListResource.TRELLO_BASE_URL}/lists/{list_id}/name?token={TOKEN}', json={
+            "value": "list 1 edit"
+        })
+
+        new_list_name = update_list_response.json()['name']
+
+        self.assertEqual(new_list_name, 'list 1 edit')
+        self.delete_board(board_id)
+
     @staticmethod
     def create_new_board(name, description):
         new_trello_board = {
